018cfa878de578b8d565d6e093462e9d01b6970f
JustinJasa <jjasa.2000@gmail.com>
2021-08-31 18:42:08 +1000

    hero stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Nav from './Components/Nav';
+import Nav from './components/Nav';
+import Hero from './components/Hero';
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<Hero />
 		</div>
 	);
 }
